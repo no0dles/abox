b863195bd3efac49f04aa2d7797c8f90026f21cd
Pascal Bertschi <pascal.bertschi@hotmail.com>
2016-10-02 22:57:34 +0200

    added istanbul coverage

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,10 @@ var bump = require('gulp-bump');
 var filter = require('gulp-filter');
 var tag_version = require('gulp-tag-version');
 
+var mocha = require("gulp-mocha");
+var istanbul = require("gulp-istanbul");
+var open = require("gulp-open");
+var remapIstanbul = require("remap-istanbul/lib/gulpRemapIstanbul");
 
 function inc(importance) {
   return gulp.src(['./package.json'])
@@ -17,3 +21,47 @@ function inc(importance) {
 gulp.task('bump-patch', function() { return inc('patch'); });
 gulp.task('bump-minor', function() { return inc('minor'); });
 gulp.task('bump-major', function() { return inc('major'); });
+
+
+
+gulp.task("pre-coverage", function () {
+  return gulp.src(["dist/**/*.js", "!dist/**/*.spec.js"])
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire());
+});
+
+gulp.task("build-coverage", ["pre-coverage"], function () {
+  return gulp.src("dist/**/*.spec.js", {read: false})
+    .pipe(mocha({
+      reporter: "dot",
+      require: ["source-map-support/register"]
+    }))
+    .pipe(istanbul.writeReports({
+      reporters: [ "json" ],
+      reportOpts: {
+        json: {dir: "dist/coverage/json", file: "coverage.json" }
+      }
+    }));
+});
+
+gulp.task("coverage", ["build-coverage"], function () {
+  return gulp.src("dist/coverage/json/coverage.json")
+    .pipe(remapIstanbul({
+      reports: {
+        "html": "dist/coverage/html"
+      }
+    }));
+});
+gulp.task("open-coverage", ["coverage"], function () {
+  return gulp.src('dist/coverage/html/index.html')
+    .pipe(open());
+});
+
+
+gulp.task("test", function () {
+  return gulp.src("dist/test/**/*.spec.js", {read: false})
+    .pipe(mocha({
+      reporter: "spec",
+      require: ["source-map-support/register"]
+    }));
+});
